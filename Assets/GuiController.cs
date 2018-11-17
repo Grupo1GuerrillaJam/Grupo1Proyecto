62374eb7ba6b2e6669ab3277a7867f15d406adbf
Grupo1GuerrillaJam <45125975+Grupo1GuerrillaJam@users.noreply.github.com>
2018-11-17 17:28:23 +0100

    Add files via upload
    first commit

---
+++ Assets/GuiController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuiController : MonoBehaviour {
+
+    public Text speedText;
+    public Text pointsText;
+
+    private GameObject player;
+    private Rigidbody _rigi;
+    private int speed;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        _rigi = player.GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        pointsText.text = "6/6 Points";
+
+        speedText.text = "Km/h";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(_rigi.velocity.x < 0)
+        {
+            speedText.text = - _rigi.velocity.x + "Km/h";
+        }
+
+
+	}
+}
---
+++ Assets/Scenes/CarController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+	Rigidbody2D _rigi;
+
+	[SerializeField]
+	private float speed;					//Velocidad actual
+	public float slowSpeed;					//Velocidad normal
+	public float fastSpeed;					//Velocidad con sprint
+
+
+	float speedX, speedY, sprintX, sprintY;	//Velocidad en coordenada X e Y, ademas de el sprint
+	bool sprint;							//El sprint se activa;
+
+	private void Awake()
+	{
+		_rigi = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+
+		sprint = false;
+		speed = slowSpeed;
+		speedX = speed;
+		speedY = 0;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+		//Si se pulsa el espacio, se pone el boleano a true y se activa el sprint
+		//Si se suelta el espacio, se pone el boleano a false
+		if (Input.GetKey (KeyCode.Space)) {
+			sprint = true;
+		} else if (Input.GetKeyUp (KeyCode.Space)) {
+			sprint = false;
+		}
+
+		//Se iguala la velocidad con la velocidad de sprint de coordenadas
+		if (sprint) 
+			sprintX = sprintY = fastSpeed;
+		else
+			sprintX = sprintY = slowSpeed;
+
+		//Al clickar las flechas se cambia la velocidad entre la velocidad en X y en Y
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			speedY = speed;
+			speedX = 0;
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			speedY = 0;
+			speedX = -speed;
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) 
+		{
+			speedY = -speed;
+			speedX = 0;
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			speedY = 0;
+			speedX = speed;
+		}
+
+		//Movimiento calculado por la velocidad en la cordenada, el sprint y el Time.DeltaTime
+		Vector2 move = new Vector2 (speedX * sprintX *Time.deltaTime, speedY * sprintY* Time.deltaTime);
+		_rigi.MovePosition (_rigi.position + move);
+	}
+}
